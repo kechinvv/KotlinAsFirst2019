e50c9ae298f948937de21e80305267cd88245f25
kechin.vv <kechinv1@rambler.ru>
2019-09-23 20:28:29 +0300

    readyright

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -40,15 +40,14 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int {
-    return when {
-        (month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10) || (month == 12) -> 31
-        (year % 400 == 0) && (month == 2) -> 29
-        ((year % 4 != 0) || (year % 100 == 0)) && (month == 2)  -> 28
-        (year % 4 == 0) && (month == 2) -> 29
-        else -> 30
-    }
+fun daysInMonth(month: Int, year: Int): Int = when {
+    (month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10) || (month == 12) -> 31
+    (year % 400 == 0) && (month == 2) -> 29
+    ((year % 4 != 0) || (year % 100 == 0)) && (month == 2) -> 28
+    (year % 4 == 0) && (month == 2) -> 29
+    else -> 30
 }
+
 
 /**
  * Средняя
